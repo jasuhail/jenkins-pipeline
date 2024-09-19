202bcadb5f6dceed4b3aa5f84bac0378526f8b8d
Ameer Suhail <80986120+jasuhail@users.noreply.github.com>
2024-09-19 17:00:38 +0530

    update

--- 2-terraform-eks-deployment/backend.tf
+++ 2-terraform-eks-deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
-    region = "eu-west-3"
+    bucket = "jasuuk-state-file"
+    region = "eu-west-1"
     key = "eks/terraform.tfstate"
   }
 }
