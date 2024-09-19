0eb8281ce4ae0e81234ac9ab51bef16af15b93e4
Ameer Suhail <80986120+jasuhail@users.noreply.github.com>
2024-09-19 11:34:06 +0530

    update

--- 1-terraform-jenkins-server/jenkins-script.sh
+++ 1-terraform-jenkins-server/jenkins-script.sh
@@ -7,9 +7,10 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-21-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
+sudo systemctl daemon-reload
 sudo systemctl start jenkins
 
 # install git
@@ -23,6 +24,6 @@ sudo yum -y install terraform
 
 # install kubectl
 
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.30.0/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
