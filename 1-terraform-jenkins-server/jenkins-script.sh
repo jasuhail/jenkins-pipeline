1720606d22a2070a7bf03feefff32b60b2cf1df2
Ameer Suhail <80986120+jasuhail@users.noreply.github.com>
2024-09-19 13:19:00 +0530

    java source

--- 1-terraform-jenkins-server/jenkins-script.sh
+++ 1-terraform-jenkins-server/jenkins-script.sh
@@ -7,7 +7,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-sudo dnf install java-21-amazon-corretto -y
+sudo yum install java-17-amazon-corretto-devel.x86_64
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl daemon-reload
